e0f33491fc8dc86fc3d7ba83a7ba453b468f5c98
brauliomac <brauliomachado100@gmail.com>
2026-05-17 22:56:12 +0100

    back and front working

---
+++ back-laravel/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+  //  return $request->user();
+//})->middleware('auth:sanctum');
+
+//Route::get('/users', [UserController::class, 'index']);
+//Route::post('/users', [UserController::class, 'store']);
+
+Route::apiResource('/users', UserController::class);
